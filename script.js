8c10cd386083c32e66146bca0a42f6a0e1367820
mubanga-1 <mubanga.s10@gmail.com>
2025-12-25 10:45:36 +0200

    Implement book creation functionality

--- script.js
+++ script.js
@@ -5,12 +5,19 @@ addBtn.addEventListener("click", () => {
     bookFormContainer.style.display = "block";
 });
 
-const myLibrary = [];
+const myLibrary = [
+    new Book("Hamlet", "William Shakespeare", "https://i.imgur.com/7AOVRQk.jpeg", 317),
+    new Book("The Great Gatsby", "F. Scott Fitzgerald", "https://i.imgur.com/fQAvXre.jpeg", 300),
+];
 
-function Book() {
+function Book(title, author, imageUrl, pages) {
   // the constructor...
+  this.title = title;
+  this.author = author;
+  this.imageUrl = imageUrl;
+  this.pages = pages;
 }
 
-function addBookToLibrary() {
+function addBookToLibrary(title, author, imageUrl, pages) {
   // take params, create a book then store it in the array
 }
